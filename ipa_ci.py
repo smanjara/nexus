d0fdd4c2cfe56b14b30fc2e96e40f7302b40e783
Gowrishankar Rajaiyan <gsr@redhat.com>
2014-08-26 01:13:57 +0530

    Add logging
    Signed-off-by: Gowrishankar Rajaiyan <gsr@redhat.com>

--- ipa_ci.py
+++ ipa_ci.py
@@ -18,6 +18,10 @@
 import sys
 import os
 import paramiko
+import logging
+
+logging.basicConfig(level=logging.INFO)
+LOG = logging.getLogger(__name__)
 
 username = "root"
 password = "whatever"
@@ -25,25 +29,30 @@ git_clone = "git clone <git-url>"
 
 # checks if the list is empty
 def check_empty_list():
+    LOG.info('Checking if EXISTING_NODES variable is empty')
     host_in = os.environ.get('EXISTING_NODES')
     if not host_in:
-        print "List is empty!"
+        LOG.error('List is empty!')
         sys.exit(1)
 
 # print the first node as master node
 def ipa_topology():
     my_node = tuple(os.environ.get('EXISTING_NODES').split(","))
     if len(my_node) == 1:
-        print "I have only %s and it is my MASTER." % my_node[0]
+        LOG.info("I have only %s and it is my MASTER." % my_node[0])
+
         # creats a new SSHClient object and then calls connect()
         ssh = paramiko.SSHClient()
         # "paramiko.AutoAddPolicy()" which will auto-accept unknown keys.
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(my_node[0], username=username,
                     password=password)
+        LOG.info("Executing command %s" % git_clone)
         stdin, stdout, stderr = ssh.exec_command(git_clone)
+
         for line in stdout.read().splitlines():
-            print 'host: %s: %s' % (my_node[0], line)
+            LOG.info('host: %s: %s' % (my_node[0], line))
+        LOG.info("Closing ssh connection.")
         ssh.close()
 
 check_empty_list()
