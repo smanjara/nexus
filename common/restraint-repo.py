0d1a0498529f3af701142dab5597a236b24032b9
Niranjan Mallapadi <mrniranjan@redhat.com>
2014-10-09 17:09:38 +0530

    Modify restraint to an working version
    Rename recent restraint.py to restraint-repo.py

--- common/restraint-repo.py
+++ common/restraint-repo.py
@@ -18,6 +18,8 @@ import subprocess
 from common.nodes import ExistingNodes
 from common.config import SetupConfig
 import ConfigParser
+import json
+from pprint import pprint
 
 
 class Restraint():
@@ -29,28 +31,49 @@ class Restraint():
         self.password = global_config.get('global', 'password')
         self.r_pkgs = global_config.get('restraint', 'remove_pkgs')
         self.i_pkgs = global_config.get('restraint', 'install_pkgs')
+        self.workspace = global_config.get('global', 'workspace')
 
 
     def restraint_repo(self):
         """downloads restraint repo file into /etc/yum.repos.d/"""
-        # TODO: check the OS and download its respective repo file instead of
-        # hardcoding el6.repo
-        # https://github.com/gsr-shanks/ci-utilities/issues/8
-        resources = ExistingNodes("EXISTING_NODES")
-        my_nodes = resources.identify_nodes()
 
-        repo_url = "http://file.bos.redhat.com/~bpeck/restraint/el6.repo"
-        get_repo = ("wget %s -O /etc/yum.repos.d/restraint.repo" % repo_url)
+        resources_file = os.path.join(self.workspace, "resources.json")
+        print resources_file
 
-        for node in my_nodes:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(node, username=self.username,
+        with open(resources_file) as json_file:
+            data = json.load(json_file)
+            version = data["family"]
+            if version == "RedHatEnterpriseLinux6":
+                     repo_url = "http://file.bos.redhat.com/~bpeck/restraint/el6.repo"
+                     get_repo = ("wget %s -O /etc/yum.repos.d/restraint.repo" % repo_url)
+            elif version == "RedHatEnterpriseLinux7":
+                    repo_url = "http://file.bos.redhat.com/~bpeck/restraint/el7.repo"
+                    get_repo = ("wget %s -O /etc/yum.repos.d/restraint.repo" % repo_url)
+            elif version == "RedHatEnterpriseLinux4":
+                    repo_url = "http://file.bos.redhat.com/~bpeck/restraint/el4.repo"
+                    get_repo = ("wget %s -O /etc/yum.repos.d/restraint.repo" % repo_url)
+            elif version == "RedHatEnterpriseLinux5":
+                    repo_url = "http://file.bos.redhat.com/~bpeck/restraint/el5.repo"
+                    get_repo = ("wget %s -O /etc/yum.repos.d/restraint.repo" % repo_url)
+            elif version == "Fedora19":
+                    repo_url = "http://file.bos.redhat.com/~bpeck/restraint/fc19.repo"
+                    get_repo = ("wget %s -O /etc/yum.repos.d/restraint.repo" % repo_url)
+            elif version == "Fedora20":
+                    repo_url = "http://file.bos.redhat.com/~bpeck/restraint/fc20.repo"
+                    get_repo = ("wget %s -O /etc/yum.repos.d/restraint.repo" % repo_url)
+
+            node = data["system"]
+
+            for node in json_file:
+               ssh = paramiko.SSHClient()
+               ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+               ssh.connect(node, username=self.username,
                         password=self.password)
-            util.log.info("Executing command %s" % get_repo)
-            stdin, stdout, stderr = ssh.exec_command(get_repo)
-            for line in stdout.read().splitlines():
-                util.log.info('host: %s: %s' % (node, line))
+               util.log.info("Executing command %s" % get_repo)
+               stdin, stdout, stderr = ssh.exec_command(get_repo)
+               for line in stdout.read().splitlines():
+                  util.log.info('host: %s: %s' % (node, line))
+
 
     def remove_rhts_python(self):
         """remove rhts-python as it conflicts with restraint-rhts"""
@@ -134,4 +157,3 @@ class Restraint():
         fd = open(junit_xml, "w")
         fd.write(stdout)
         fd.close()
-
