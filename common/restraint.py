53d91545863e8bf8c13ef8bc3f390182c1cee663
Niranjan Mallapadi <mrniranjan@redhat.com>
2014-11-23 21:05:45 +0530

    Add SSHClient class which inherits sshclient

--- common/restraint.py
+++ common/restraint.py
@@ -19,17 +19,44 @@ from common.nodes import ExistingNodes
 from common.config import SetupConfig
 import ConfigParser
 
-
+class SSHClient(paramiko.SSHClient):
+    """ This class Inherits paramiko.SSHClient and implements client.exec_commands
+     channel.exec_command """
+	
+    def __init__(self):
+	global_config = ConfigParser.SafeConfigParser()
+	global_config.read("etc/global.conf")
+	util.log.info (global_config.sections())
+	self.username = global_config.get('global', 'username')
+	self.password = global_config.get('global', 'password')
+	paramiko.SSHClient.__init__(self)
+	self.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	
+    def Execute(self, nodes, args):
+	""" This Function executes commands using SSHClient.exec_commands().
+        @params: args: Commands that needs to be executed.
+		 nodes: list of hostnames on which commands should be executed
+	"""
+	for host in nodes:
+	    self.connect(host, self.username, self.password)
+            util.log.info("Executing command %s" % args)
+        try:
+	     stdin, stdout, stderr = self.exec_command(args)
+   	except paramiko.SSHException, e:
+	     print "Cannot execute %s", args
+	else:
+	     if stderr:
+	         for line in stderr.read().splitlines():
+	   	     util.log.info('host: %s %s' %(host, line))
+		     sys.exit(1)
+	     else:
+		 for line in stdout.read().splitlines():
+		      util.log.info('host: %s: %s' % (host, line))
+		
 class Restraint():
     def __init__(self):
-        global_config = ConfigParser.SafeConfigParser()
-        global_config.read("etc/global.conf")
-        util.log.info (global_config.sections())
-        self.username = global_config.get('global', 'username')
-        self.password = global_config.get('global', 'password')
         self.r_pkgs = global_config.get('restraint', 'remove_pkgs')
         self.i_pkgs = global_config.get('restraint', 'install_pkgs')
-
 
     def restraint_repo(self):
         """downloads restraint repo file into /etc/yum.repos.d/"""
@@ -48,17 +75,9 @@ class Restraint():
             repo_url = rhel7_restraint_repo
 
         get_repo = ("wget %s -O /etc/yum.repos.d/restraint.repo" % repo_url)
+	RunTask = SSHClient()
+	RunTask.Execute(my_nodes, get_repo)
 
-        #TODO use threads instead of for loop
-        for node in my_nodes:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(node, username=self.username,
-                        password=self.password)
-            util.log.info("Executing command %s" % get_repo)
-            stdin, stdout, stderr = ssh.exec_command(get_repo)
-            for line in stdout.read().splitlines():
-                util.log.info('host: %s: %s' % (node, line))
 
     def remove_rhts_python(self):
         """remove rhts-python as it conflicts with restraint-rhts"""
@@ -67,21 +86,9 @@ class Restraint():
         my_nodes = resources.identify_nodes()
 
         yum_remove = ("yum remove -y %s" % self.r_pkgs)
-
-        #TODO use threads instead of for loop
-        for node in my_nodes:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(node, username=self.username, password=self.password)
-            util.log.info("Executing command %s" % yum_remove)
-            stdin, stdout, stderr = ssh.exec_command(yum_remove)
-            for line in stdout.read().splitlines():
-                if "error" in line:
-                    util.log.error('host: %s: %s' % (node, line))
-                    sys.exit(1)
-                else:
-                    util.log.info('host: %s: %s' % (node, line))
-
+	RunTask = SSHClient()
+	RunTask.Execute(my_nodes, yum_remove)
+	
     def restraint_install(self):
         """Installs all the packages required for restraint"""
 
@@ -89,22 +96,9 @@ class Restraint():
         my_nodes = resources.identify_nodes()
 
         yum_install = ("yum install -y %s" % self.i_pkgs)
-
-        #TODO use threads instead of for loop
-        for node in my_nodes:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(node, username=self.username,
-                        password=self.password)
-            util.log.info("Executing command %s" % yum_install)
-            stdin, stdout, stderr = ssh.exec_command(yum_install)
-            for line in stdout.read().splitlines():
-                if "error" in line:
-                    util.log.error('host: %s: %s' % (node, line))
-                    sys.exit(1)
-                else:
-                    util.log.info('host: %s: %s' % (node, line))
-
+	RunTask = SSHClient()
+	RunTask.Execute(my_nodes, yum_install)
+	
     def restraint_start(self):
         """start the restraint service and chkconfig on"""
 
@@ -114,21 +108,8 @@ class Restraint():
         # TODO: move this service name to config/idm_setup.cfg
         service = ("restraintd")
         start_service = ("service %s start; chkconfig %s on" % (service, service))
-
-        #TODO use threads instead of for loop
-        for node in my_nodes:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(node, username=self.username,
-                        password=self.password)
-            util.log.info("Executing command %s" % start_service)
-            stdin, stdout, stderr = ssh.exec_command(start_service)
-            for line in stdout.read().splitlines():
-                if "error" in line:
-                    util.log.error('host: %s: %s' % (node, line))
-                    sys.exit(1)
-                else:
-                    util.log.info('host: %s: %s' % (node, line))
+	RunTask = SSHClient()
+	RunTask.Execute(my_nodes, start_service)
 
     def restraint_junit(self, x):
         """convert job.xml to junit.xml"""
