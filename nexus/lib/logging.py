b47ce69200dc59bfc05325e5afa4a154d49b0cbb
Gowrishankar Rajaiyan <gsr@redhat.com>
2015-01-05 12:29:22 +0530

    add logging.py
    Signed-off-by: Gowrishankar Rajaiyan <gsr@redhat.com>

---
+++ nexus/lib/logging.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+""" Contains common utility functions to be used internally """
+import logging
+
+log = logging.getLogger(__name__)
+
+# set Log Level
+log.setLevel(logging.DEBUG)
+
+
+# Set Stream Handler
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+# set Formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+log.addHandler(handler)
+log.propagate = False
+
