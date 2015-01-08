92010caec0c52a09cede3b02ecbf08cbf0733295
Gowrishankar Rajaiyan <gsr@redhat.com>
2015-01-09 02:39:12 +0530

    include execute restraint

--- nexus/plugins/restraint.py
+++ nexus/plugins/restraint.py
@@ -4,6 +4,7 @@ import os
 import sys
 import platform
 import glob
+import subprocess
 from nexus.lib.factory import SSHClient
 from nexus.lib.factory import Threader
 from nexus.lib import jenkins
@@ -17,8 +18,9 @@ class Restraint():
         nodes = conf_dict['jenkins']['existing_nodes']
         self.existing_nodes = [item.strip() for item in nodes.split(',')]
 
+        self.jenkins_job_name = conf_dict['jenkins']['job_name']
 
-    def copy_build_repo(self):
+    def copy_build_repo(self, host, conf_dict):
         """copy the brew build repo to all the existing nodes"""
 
         build_repo_tag = os.environ.get("BUILD_REPO_TAG")
@@ -37,7 +39,18 @@ class Restraint():
         source = build_repo_file
         destination = "/etc/yum.repos.d/" + source
 
-        return source, destination
+        ssh_c = SSHClient(hostname = host, username = \
+                                  self.username, password = self.password)
+        ssh_c.CopyFiles(source, destination)
+
+    def my_build_repo(self, host, conf_dict):
+
+        source = self.build_repo
+        destination = "/etc/yum.repos.d/" + source
+
+        ssh_c = SSHClient(hostname = host, username = \
+                                  self.username, password = self.password)
+        ssh_c.CopyFiles(source, destination)
 
 
     def restraint_setup(self, host, conf_dict):
@@ -46,7 +59,6 @@ class Restraint():
         Install restraint rpms and start its service.
         """
 
-        print host
         ssh_c = SSHClient(hostname = host, username = \
                                   self.username, password = self.password)
         stdin, stdout, stderr = ssh_c.ExecuteCmd('python -c "import platform; \
@@ -77,8 +89,44 @@ class Restraint():
         stdin, stdout, stderr = ssh_c.ExecuteCmd(start_service_cmd)
         for line in stdout.read().splitlines(): print line
 
-        source, destination = self.copy_build_repo()
-        ssh_c.CopyFiles(source, destination)
+    def restraint_update_xml(self):
+
+        node = 0
+        host_num = 1
+        host_recipe = []
+        while node < len(self.existing_nodes):
+            if os.path.exists(self.restraint_xml):
+                host_num = str(host_num)
+                hostname = ("hostname" + host_num);
+                host_num = int(host_num)
+                j = open(self.restraint_xml, 'r').read()
+                m = j.replace(hostname, (self.existing_nodes[node]))
+                f = open(self.restraint_xml, 'w')
+                f.write(m)
+                f.close()
+
+                mystr = "-t" + " " + str(host_num) + '=' + self.existing_nodes[node]
+
+                host_recipe.append(mystr)
+                self.restraint_hosts = " ".join(host_recipe)
+                node = node + 1
+                host_num = host_num + 1
+            else:
+                sys.exit(2)
+
+    def execute_restraint(self):
+
+        subprocess.check_call(['cat', self.restraint_xml])
+        if len(self.existing_nodes) == 1:
+            host1 = ("1=%s:8081" % self.existing_nodes[0])
+            returncode = subprocess.check_call(['restraint', '-j', \
+                         self.restraint_xml, '-t', host1, '-v', '-v'])
+            return returncode
+        else:
+            rest_command = "restraint" + " " + "-j" + " " + self.restraint_xml \
+                            + " " + self.restraint_hosts + " " + "-v" + " " + "-v"
+            returncode = subprocess.check_call(rest_command.split(), shell=False)
+            return returncode
 
     def restraint_junit(self):
         """convert job.xml to junit.xml"""
@@ -100,12 +148,38 @@ class Restraint():
         fd.write(stdout)
         fd.close()
 
-    def run_restraint(self, conf_dict):
+    def run_restraint(self, options, conf_dict):
         """
         Call restraint_setup function using threads per host.
         """
-
-        print self.existing_nodes
         threads = Threader()
-        threads.gather_results([threads.get_item(self.restraint_setup, host, conf_dict) for host in \
-                            self.existing_nodes])
+
+        if options.restraint_xml is None:
+            self.jenkins_workspace = conf_dict['jenkins']['workspace']
+            self.restraint_xml_loc = conf_dict['restraint_jobs'][self.jenkins_job_name]
+            self.restraint_xml = os.path.join(self.jenkins_workspace, \
+                                 self.restraint_xml_loc)
+            threads.gather_results([threads.get_item(self.restraint_setup, \
+                                    host, conf_dict) for host in self.existing_nodes])
+        else:
+            self.restraint_xml = options.restraint_xml
+            threads = Threader()
+            threads.gather_results([threads.get_item(self.restraint_setup, \
+                                    host, conf_dict) for host in self.existing_nodes])
+
+        if options.build_repo is None:
+            threads.gather_results([threads.get_item(self.copy_build_repo, \
+                                    host, conf_dict) for host in self.existing_nodes])
+        else:
+            self.build_repo = options.build_repo
+            threads.gather_results([threads.get_item(self.my_build_repo, \
+                                    host, conf_dict) for host in self.existing_nodes])
+
+        if len(self.existing_nodes) == 1:
+            print self.restraint_xml
+            self.restraint_update_xml()
+            self.execute_restraint()
+        else:
+            print self.restraint_xml
+            self.execute_restraint()
+            self.restraint_update_xml()
