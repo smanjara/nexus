ac7235bfdbf146e72fabe21027524d16668d44dc
Gowrishankar Rajaiyan <gsr@redhat.com>
2015-01-15 00:08:47 +0530

    Enable logging for git plugin

--- nexus/plugins/git.py
+++ nexus/plugins/git.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
+# Copyright (c) 2015 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing
+# to use, modify, copy, or redistribute it subject to the terms
+# and conditions of the GNU General Public License version 2.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
 
 import os
 from subprocess import call
+from nexus.lib import logger
 
 class Git():
 
@@ -15,15 +26,19 @@ class Git():
         else:
             self.git_project = options.project
 
+        logger.log.info("GIT project name: %s" % self.git_project)
+
         if options.repo is None:
             self.git_repo = conf_dict['git']['git_repo_url']
         else:
             self.git_repo = options.repo
+        logger.log.info("GIT repo URL: %s" % self.git_repo)
 
         if options.branch is None:
             self.git_branch = conf_dict['git']['git_get_branch']
         else:
             self.git_branch = options.branch
+        logger.log.info("GIT repo branch: %s" % self.git_branch)
 
         if options.tar is None:
             self.git_tar = conf_dict['git']['git_tar_out']
@@ -36,8 +51,11 @@ class Git():
         """
 
         if not os.path.exists(self.git_project):
+            logger.log.info("Project directory does not exist, creating one")
             os.makedirs(self.git_project)
         git_remote = "--remote=" + self.git_repo
         git_branch = self.git_branch + ":"
+        logger.log.info("Archiving git branch...")
         call(["git", "archive", "-v", git_remote, git_branch, "-o", self.git_tar])
+        logger.log.info("Extracting the git archive...")
         call(["tar", "-xf", self.git_tar, "-C", self.git_project])
